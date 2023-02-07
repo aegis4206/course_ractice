75ffd3c9017ac74223024722747b5ebc73b3d526
aegis4206 <123101758+aegis4206@users.noreply.github.com>
2023-02-07 17:23:42 +0800

    Update firebase.js

--- SOCIAL-COOL/src/utils/firebase.js
+++ SOCIAL-COOL/src/utils/firebase.js
@@ -4,12 +4,7 @@ import { initializeApp } from 'firebase/app';
 // import firebase from 'firebase/compat/app'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDE4Ow2c41a4NTgjBJTj06UQIKG4QrsJK4",
-  authDomain: "social-cool-b8cbe.firebaseapp.com",
-  projectId: "social-cool-b8cbe",
-  storageBucket: "social-cool-b8cbe.appspot.com",
-  messagingSenderId: "493828729126",
-  appId: "1:493828729126:web:96c682c8fa76752a0239e7"
+ 
 };
 
 // const firebaseApp=initializeApp(firebaseConfig);
